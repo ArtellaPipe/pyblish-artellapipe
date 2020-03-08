a5373f17febfd7e371d6d488200a9be13b212c31
Tomi Poveda <tpovedatd@gmail.com>
2020-03-08 01:38:59 +0100

    Added shaders checks for Maya

---
+++ artellapipe/libs/pyblish/validators/modeling/shaders/shaders.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains geometry shaders validator implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+
+import tpDcc as tp
+
+import pyblish.api
+
+
+class ValidateGeometryShaders(pyblish.api.InstancePlugin):
+    """
+    Checks if current scene has geometries with shaders applied
+    """
+
+    label = 'Geometry - Shaders'
+    order = pyblish.api.ValidatorOrder
+    hosts = ['maya']
+    optional = False
+
+    def process(self, instance):
+
+        import maya.cmds as cmds
+
+        node = instance.data.get('node', None)
+        assert tp.Dcc.object_exists(node), 'No valid node found in current instance: {}'.format(instance)
+
+        nodes_to_check = self._nodes_to_check(node)
+        assert nodes_to_check, 'No Nodes to check found!'
+
+        shaders = dict()
+        for node in nodes_to_check:
+            shading_groups = None
+            shape = cmds.listRelatives(node, shapes=True, fullPath=True)
+            if cmds.nodeType(shape) == 'mesh':
+                if shape:
+                    shading_groups = cmds.listConnections(shape, type='shadingEngine')
+                shading_group_to_check = shading_groups[0]
+                if not shading_group_to_check == 'initialShadingGroup':
+                    if shading_group_to_check not in shaders:
+                        shaders[shading_group_to_check] = list()
+                    shaders[shading_group_to_check].append(node)
+
+        assert not shaders, 'Geometries with applied shaders found: {}'.format(shaders)
+
+    def _nodes_to_check(self, node):
+
+        valid_nodes = list()
+        nodes = tp.Dcc.list_children(node=node, all_hierarchy=True, full_path=True, children_type='transform')
+        if not nodes:
+            nodes = [node]
+        else:
+            nodes.append(node)
+
+        for node in nodes:
+            shapes = tp.Dcc.list_shapes(node=node, full_path=True)
+            if not shapes:
+                continue
+            valid_nodes.append(node)
+
+        return valid_nodes
