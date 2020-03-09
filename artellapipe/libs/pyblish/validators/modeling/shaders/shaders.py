cac4cd6664da68fa5fb37fe53f1b2035957496c1
Tomas Poveda <tpovedatd@gmail.com>
2020-03-09 02:46:13 +0100

    Added custom action to fix shaders validator

--- artellapipe/libs/pyblish/validators/modeling/shaders/shaders.py
+++ artellapipe/libs/pyblish/validators/modeling/shaders/shaders.py
@@ -18,6 +18,41 @@ import tpDcc as tp
 import pyblish.api
 
 
+class ApplyDefaultShader(pyblish.api.Action):
+    label = 'Apply Default Shader'
+    on = 'failed'
+
+    def process(self, context, plugin):
+        if not tp.is_maya():
+            self.log.warning('Select Vertex Poles Action is only available in Maya!')
+            return False
+
+        import tpDcc.dccs.maya as maya
+
+        for instance in context:
+            if not instance.data['publish']:
+                continue
+
+            node = instance.data.get('node', None)
+            assert node and tp.Dcc.object_exists(node), 'No valid node found in current instance: {}'.format(instance)
+
+            apply_default_shader = instance.data.get('apply_default_shader', False)
+            if not apply_default_shader:
+                continue
+
+            maya.cmds.undoInfo(openChunk=True)
+
+            try:
+                node_shapes = maya.cmds.listRelatives(node, shapes=True, fullPath=True)
+                maya.cmds.sets(node_shapes, edit=True, forceElement='initialShadingGroup')
+            except Exception as exc:
+                self.log.error('Error while applying default shader on shapes: {} | {}'.format(node_shapes, exc))
+            finally:
+                maya.cmds.undoInfo(openChunk=False)
+
+        return True
+
+
 class ValidateGeometryShaders(pyblish.api.InstancePlugin):
     """
     Checks if current scene has geometries with shaders applied
@@ -27,10 +62,11 @@ class ValidateGeometryShaders(pyblish.api.InstancePlugin):
     order = pyblish.api.ValidatorOrder
     hosts = ['maya']
     optional = False
+    actions = [ApplyDefaultShader]
 
     def process(self, instance):
 
-        import maya.cmds as cmds
+        import tpDcc.dccs.maya as maya
 
         node = instance.data.get('node', None)
         assert tp.Dcc.object_exists(node), 'No valid node found in current instance: {}'.format(instance)
@@ -38,18 +74,24 @@ class ValidateGeometryShaders(pyblish.api.InstancePlugin):
         nodes_to_check = self._nodes_to_check(node)
         assert nodes_to_check, 'No Nodes to check found!'
 
+        nodes_with_shaders = list()
         shaders = dict()
-        for node in nodes_to_check:
+        for check_node in nodes_to_check:
             shading_groups = None
-            shape = cmds.listRelatives(node, shapes=True, fullPath=True)
-            if cmds.nodeType(shape) == 'mesh':
+            shape = maya.cmds.listRelatives(check_node, shapes=True, fullPath=True)
+            if maya.cmds.nodeType(shape) == 'mesh':
                 if shape:
-                    shading_groups = cmds.listConnections(shape, type='shadingEngine')
+                    shading_groups = maya.cmds.listConnections(shape, type='shadingEngine')
                 shading_group_to_check = shading_groups[0]
                 if not shading_group_to_check == 'initialShadingGroup':
                     if shading_group_to_check not in shaders:
                         shaders[shading_group_to_check] = list()
-                    shaders[shading_group_to_check].append(node)
+                    shaders[shading_group_to_check].append(check_node)
+                    if check_node not in nodes_with_shaders:
+                        nodes_with_shaders.append(check_node)
+
+        if node in nodes_with_shaders:
+            instance.data['apply_default_shader'] = True
 
         assert not shaders, 'Geometries with applied shaders found: {}'.format(shaders)
 
--- artellapipe/libs/pyblish/validators/scene/noshaders.py
+++ artellapipe/libs/pyblish/validators/scene/noshaders.py
@@ -12,7 +12,66 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
+import tpDcc as tp
+
 import pyblish.api
+
+
+class CleanShaders(pyblish.api.Action):
+    label = 'Clean Shaders'
+    on = 'failed'
+
+    def process(self, context, plugin):
+        if not tp.is_maya():
+            self.log.warning('Clean Shaders Action is only available in Maya!')
+            return False
+
+        import tpDcc.dccs.maya as maya
+
+        extra_shaders = context.data.get('extra_shaders', None)
+        assert extra_shaders, 'No extra shaders found in current context: {}'.format(context)
+
+        shaders_to_delete = list()
+
+        maya.cmds.undoInfo(openChunk=True)
+
+        try:
+            for shading_group in extra_shaders:
+                shader_shapes = maya.cmds.listConnections(shading_group, type='mesh')
+                if shader_shapes:
+                    maya.cmds.sets(shader_shapes, edit=True, forceElement='initialShadingGroup')
+                shaders_to_delete.append(shading_group)
+                shaders_to_delete.extend(maya.cmds.listConnections(shading_group, type='surfaceShader') or list())
+            shaders_to_delete = list(set(shaders_to_delete))
+            for shader_to_delete in shaders_to_delete:
+                if not maya.cmds.objExists(shader_to_delete):
+                    continue
+                tp.Dcc.delete_object(shader_to_delete)
+        except Exception as exc:
+            self.log.error('Error while cleaning shaders from scene: {}'.format(exc))
+        finally:
+            maya.cmds.undoInfo(closeChunk=True)
+
+
+class SelectShaders(pyblish.api.Action):
+    label = 'Select Shaders'
+    on = 'failed'
+
+    def process(self, context, plugin):
+        if not tp.is_maya():
+            self.log.warning('Select Shaders Action is only available in Maya!')
+            return False
+
+        import tpDcc.dccs.maya as maya
+
+        extra_shaders = context.data.get('extra_shaders', None)
+        assert extra_shaders, 'No extra shaders found in current context: {}'.format(context)
+
+        shaders_to_select = list()
+        for shading_group in extra_shaders:
+            shaders_to_select.extend(maya.cmds.listConnections(shading_group, type='surfaceShader'))
+        shaders_to_select = list(set(shaders_to_select))
+        tp.Dcc.select_object(shaders_to_select)
 
 
 class ValidateNoShadersInScene(pyblish.api.ContextPlugin):
@@ -20,10 +79,11 @@ class ValidateNoShadersInScene(pyblish.api.ContextPlugin):
     Checks if current scene has shaders (apart from the standard ones)
     """
 
-    label = 'Check Scene Shaders'
+    label = 'Shaders - Check Scene Shaders'
     order = pyblish.api.ValidatorOrder
     hosts = ['maya']
     optional = False
+    actions = [SelectShaders, CleanShaders]
 
     def process(self, context):
 
@@ -35,5 +95,8 @@ class ValidateNoShadersInScene(pyblish.api.ContextPlugin):
             if shading_node not in ['initialParticleSE', 'initialShadingGroup']:
                 if shading_node not in extra_shaders:
                     extra_shaders.append(shading_node)
+
+        if extra_shaders:
+            context.data['extra_shaders'] = extra_shaders
 
         assert not extra_shaders, 'Current scene has non default shaders: "{}"'.format(extra_shaders)
