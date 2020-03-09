f6074da234f9e04631fa4fa26c28be3059ccf91e
Tomas Poveda <tpovedatd@gmail.com>
2020-03-09 02:43:01 +0100

    Now geometyr collector collect nodes using long names

--- artellapipe/libs/pyblish/collectors/modeling/collectgeometry.py
+++ artellapipe/libs/pyblish/collectors/modeling/collectgeometry.py
@@ -21,10 +21,12 @@ class CollectGeometry(pyblish.api.ContextPlugin):
 
         assert project, 'Project not found'
 
-        geo_meshes = cmds.ls(type='mesh')
-        geo_transforms = [cmds.listRelatives(mesh, parent=True)[0] for mesh in geo_meshes]
+        geo_meshes = cmds.ls(type='mesh', long=True)
+        geo_transforms = [cmds.listRelatives(mesh, parent=True, fullPath=True)[0] for mesh in geo_meshes]
 
         for node in geo_transforms:
-            instance = context.create_instance(node, project=project)
+            node_name = node.split('|')[-1].split(':')[-1]
+            instance = context.create_instance(node_name, project=project)
+            instance.data['icon'] = 'cube'
             instance.data['node'] = node
             instance.data['family'] = 'geometry'
