4b6bb82aa0e806bd9fc7a7d34d6bcec2a6dbeb1b
Tomas Poveda <tpovedatd@gmail.com>
2020-03-09 02:44:36 +0100

    Added custom action to fix display layer validators

--- artellapipe/libs/pyblish/validators/modeling/general/displaylayers.py
+++ artellapipe/libs/pyblish/validators/modeling/general/displaylayers.py
@@ -18,6 +18,44 @@ import tpDcc as tp
 import pyblish.api
 
 
+class RemoveFromDisplayLayer(pyblish.api.Action):
+    label = 'Remove From Display Layer'
+    on = 'failed'
+
+    def process(self, context, plugin):
+        if not tp.is_maya():
+            self.log.warning('Remove From Display Layer Action is only available in Maya!')
+            return False
+
+        import tpDcc.dccs.maya as maya
+
+        maya.cmds.undoInfo(openChunk=True)
+
+        try:
+            for instance in context:
+                if not instance.data['publish']:
+                    continue
+
+                node = instance.data.get('node', None)
+                assert node and tp.Dcc.object_exists(node), 'No valid node found in current instance: {}'.format(instance)
+
+                layer = maya.cmds.listConnections(node, type='displayLayer')
+                if not layer:
+                    continue
+
+                maya.cmds.editDisplayLayerMembers('defaultLayer', node)
+                layer = maya.cmds.listConnections(node, type='displayLayer')
+                if layer and layer != 'defaultLayer':
+                    self.log.error('Impossible to remove node from displayLayer: {}'.format(node, layer))
+                    continue
+        except Exception as exc:
+            self.log.error('Error while removing nodes from display layers: {}'.format(exc))
+        finally:
+            maya.cmds.undoInfo(closeChunk=False)
+
+        return True
+
+
 class ValidateGeometryDisplayLayers(pyblish.api.InstancePlugin):
     """
     Checks if current scene has geometries added to a display layer
@@ -27,10 +65,11 @@ class ValidateGeometryDisplayLayers(pyblish.api.InstancePlugin):
     order = pyblish.api.ValidatorOrder
     hosts = ['maya']
     optional = False
+    actions = [RemoveFromDisplayLayer]
 
     def process(self, instance):
 
-        import maya.cmds as cmds
+        import tpDcc.dccs.maya as maya
 
         node = instance.data.get('node', None)
         assert tp.Dcc.object_exists(node), 'No valid node found in current instance: {}'.format(instance)
@@ -40,7 +79,7 @@ class ValidateGeometryDisplayLayers(pyblish.api.InstancePlugin):
 
         layers = dict()
         for node in nodes_to_check:
-            layer = cmds.listConnections(node, type='displayLayer')
+            layer = maya.cmds.listConnections(node, type='displayLayer')
             if layer:
                 if layer not in layers:
                     layers[layer] = list()
--- artellapipe/libs/pyblish/validators/scene/displaylayers.py
+++ artellapipe/libs/pyblish/validators/scene/displaylayers.py
@@ -14,16 +14,46 @@ __email__ = "tpovedatd@gmail.com"
 
 import pyblish.api
 
+import tpDcc as tp
+
+
+class RemoveDisplayLayers(pyblish.api.Action):
+    label = 'Remove Display Layers'
+    on = 'failed'
+
+    def process(self, context, plugin):
+        if not tp.is_maya():
+            self.log.warning('Remove From Display Layer Action is only available in Maya!')
+            return False
+
+        import tpDcc.dccs.maya as maya
+
+        maya.cmds.undoInfo(openChunk=True)
+
+        try:
+            display_layers = maya.cmds.ls(type='displayLayer')
+            for display_layer in display_layers:
+                if display_layer == 'defaultLayer':
+                    continue
+                maya.cmds.delete(display_layer)
+        except Exception as exc:
+            self.log.error('Error while removing display layers: {}'.format(exc))
+        finally:
+            maya.cmds.undoInfo(closeChunk=False)
+
+        return True
+
 
 class ValidateDisplayLayers(pyblish.api.ContextPlugin):
     """
     Checks if current scene has display layers
     """
 
-    label = 'Display Layers in Scene'
+    label = 'Scene - Display Layers in Scene'
     order = pyblish.api.ValidatorOrder
     hosts = ['maya']
     optional = False
+    actions = [RemoveDisplayLayers]
 
     def process(self, context):
 
